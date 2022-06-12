f0df9d2f019dcab0d0cb1aa9058748ee7e83df40
adhe7x <105721068+adhe7x@users.noreply.github.com>
2022-06-12 19:19:07 +0700

    Edited by Adhi Prasetyo

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,44 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+// Fungsi mengambil message sesuai dengan struktur data Json
+
+const splitMessage = (dataJson) => {
+  
+  let arrayMsg = [];
+
+  if (dataJson.message !== undefined) {
+    arrayMsg = dataJson.message.split(" ");
+    return arrayMsg[arrayMsg.length - 1];
+  };
+
+  if (dataJson[0].message !== undefined) {
+    arrayMsg = dataJson[0].message.split(" ");
+    return arrayMsg[arrayMsg.length - 1];
+  };
+
+  if (dataJson[0].data.message !== undefined) {
+    arrayMsg = dataJson[0].data.message.split(" ");
+    return arrayMsg[arrayMsg.length - 1];
+  };
+};
+
+async function bacaData(fnCallback) {
+  let files = [file1, file2, file3];
+  let hasilAkhir= [];
+
+  try {
+    for (const file of files) {
+    const data = await fs.promises.readFile(file, "utf-8");
+    hasilAkhir.push(splitMessage(JSON.parse(data)));
+    };
+    fnCallback(null, hasilAkhir);
+  }
+  catch(err) {
+    fnCallback(err, null);
+  };
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
